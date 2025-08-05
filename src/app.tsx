b823b76695412766ea5bace369c147a9473ccd61
Stefan Adrian Nita <stefanadrian.nita@icloud.com>
2025-08-05 14:53:04 +0200

    installed chakra-ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,12 @@
 import { createRoot } from 'react-dom/client';
+import React, { Component } from 'react';
+import { Provider } from "./components/ui/provider"
+
+class App extends Component {
+    render() {
+        return <Provider><h2>Hello from React!</h2></Provider>;
+    }
+}
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(<App />);
