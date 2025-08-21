76c2ed6230a585d9d84adb4847a62270fbe714bc
Luden02 <Luden02@users.noreply.github.com>
2025-08-21 21:45:56 +0200

    app name

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,79 @@
-import { app, BrowserWindow, screen } from "electron";
+import { app, BrowserWindow, globalShortcut, Menu } from "electron";
 import path from "path";
 import electronReloader from "electron-reloader";
+import PackageInfo from "../package.json";
 
-app.whenReady().then(() => {
-  const args = process.argv.slice(1);
-  const serve = args.includes("--serve");
-  const size = screen.getPrimaryDisplay().workAreaSize;
+const size = { minWidth: 1024, minHeight: 600 };
 
-  let win = new BrowserWindow({
-    width: size.width,
-    height: size.height,
+function createWindow() {
+  const win = new BrowserWindow({
+    width: size.minWidth,
+    height: size.minHeight,
+    minWidth: size.minWidth,
+    minHeight: size.minHeight,
+    title: `OpenOPLToolbox (${PackageInfo.version})`,
+    icon: path.join(__dirname, "assets", "applogo", "icon_512x512.png"),
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       contextIsolation: true,
     },
   });
 
+  win.removeMenu();
+  Menu.setApplicationMenu(
+    Menu.buildFromTemplate([
+      {
+        label: "OpenOPL Toolbox",
+        submenu: [
+          {
+            label: "Quit",
+            accelerator: "CmdOrCtrl+Q",
+            click: () => {
+              app.quit();
+            },
+          },
+        ],
+      },
+    ])
+  );
+
+  const args = process.argv.slice(1);
+  const serve = args.includes("--serve");
+
   if (serve) {
     electronReloader(module);
     win.loadURL("http://localhost:4200");
   } else {
-    win.loadFile(path.join(__dirname, "angular/browser/index.html"));
+    win.loadFile(
+      path.join(__dirname, "..", "angular", "browser", "index.html")
+    );
   }
+}
 
-  win.on("closed", () => {
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+app.on("browser-window-focus", function () {
+  globalShortcut.register("CommandOrControl+R", () => {
+    console.log("CommandOrControl+R is pressed: Shortcut Disabled");
   });
+  globalShortcut.register("F5", () => {
+    console.log("F5 is pressed: Shortcut Disabled");
+  });
+});
+
+app.on("browser-window-blur", function () {
+  globalShortcut.unregister("CommandOrControl+R");
+  globalShortcut.unregister("F5");
 });
